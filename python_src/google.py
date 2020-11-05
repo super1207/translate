514d1a783038d02f7f13b4979226ed75d0230df5
super1207 <pengyao1210@gmail.com>
2020-11-05 07:59:02 +0000

    some comments of google translate

--- python_src/google.py
+++ python_src/google.py
@@ -39,3 +39,66 @@ class google:
         evaljs.execute(js_code)
         tk = evaljs.TL(word)
         return tk
+    
+ """
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+
+__int32 RL(unsigned __int32 a,const wchar_t * b)
+{
+    const wchar_t t = L'a';
+    const wchar_t Yb = L'+';
+    for(size_t c = 0; c < wcslen(b) - 2;c += 3)
+    {
+        unsigned __int32 d;
+        wchar_t d1 = b[c + 2];
+        unsigned __int32 d2 = (d1 >= t) ? (unsigned __int32)d1 - 87 : (unsigned __int32) d1 - L'0';
+        d = (b[c + 1] == Yb)  ? (unsigned __int32)a >> d2 : (unsigned __int32)a << d2;
+        a = b[c] == Yb ? a + d & 4294967295 : a ^ d;
+    }
+    return a;
+}
+
+double TL(const wchar_t * a)
+{
+    using namespace std;
+    const wchar_t * k = L"";
+    unsigned __int32 b = 406644;
+    unsigned __int32 b1 = 3293161072;
+    wchar_t jd = L'.';
+    const wchar_t * $b = L"+-a^+6";
+    const wchar_t * Zb = L"+-3^+b+-f";
+    vector<unsigned __int32> e;
+    unsigned __int32 f = 0,g = 0;
+    for (; g < wcslen(a); g++) {
+        unsigned __int32 m = a[g];
+        128 > m ? e[e.push_back(0),f++] = m : (2048 > m ? e[e.push_back(0),
+                f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < wcslen(a) && 56320 == (a[g + 1] & 64512) ? (m = 65536 + ((m & 1023) << 10) + (
+                a[++g] & 1023), e[e.push_back(0),f++] = m >> 18 | 240, e[e.push_back(0),f++] = m >> 12 & 63 | 128) : e[e.push_back(0),f++] = m >> 12 | 224, e[e.push_back(0),f++] = m >> 6 & 63 | 128),
+            e[e.push_back(0),f++] = m & 63 | 128);
+    }
+    unsigned __int32 a2 = b;
+    for (f = 0; f < e.size(); f++) 
+        a2 += e[f], a2 = RL(a2, $b);
+    a2 = RL(a2, Zb);
+    a2 ^= b1 | 0;
+    0 > a2 && (a2 = (
+        a2 & 2147483647) + 2147483648);
+    a2 %= 1000000;
+    char ch[20];
+    sprintf_s(ch,20,"%d%c%d",a2,'.',(a2 ^ b));
+    double out;
+    sscanf_s(ch,"%lf",&out);
+    return out;
+}
+
+
+int main()
+{
+    wchar_t a[100] = L"hello world"; //777855.911883
+    double out = TL(a);
+    printf("%lf\n",out);  
+}
+ """
